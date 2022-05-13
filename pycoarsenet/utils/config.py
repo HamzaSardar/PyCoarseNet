4de67d923985dad02c05d6b5cab96a157e7e0753
HamzaSardar <hamzasardar360@gmail.com>
2022-05-13 18:52:44 +0100

    Added in YAML config, implemented CLI for paths
    Most parameters now controlled by an external config file. Still some
    parameters to switch over to config but this will require a more
    extensive rework of the `train_model.py` script.
    
    The training script now takes command line arguments for paths to data
    files, config files, and requires a directory to store results. This
    makes the code more generalisable and usable on other machines (with the
    view to running on HPC in the future).
    
    Further, there is a key success in this commit - the implementation of
    cyclic learning rate with the Adam optimiser led to some improved
    results. This is to be explored further.

--- pycoarsenet/utils/config.py
+++ pycoarsenet/utils/config.py
@@ -4,27 +4,24 @@ from typing import Any, Dict
 import yaml
 from dataclasses import dataclass, field, fields
 
-from .enums import eCorruption, eDecoder, eSolverFunction
-from .exceptions import SolverConsistencyError, SolverConsistencyWarning
-
 
 @dataclass
 class Config:
     _config: Dict[str, Any] = field(default_factory=dict, repr=False)
 
     # simulation parameters
-    COARSE_SPACING: float = field(init=False) 
-    FINE_SIZE: int = field(init=False) 100
-    COARSE_SIZE: int = field(init=False) 20
+    COARSE_SPACING: float = field(init=False)
+    FINE_SIZE: int = field(init=False)
+    COARSE_SIZE: int = field(init=False)
 
     # training parameters
-    N_EPOCHS: int = field(init=False) 100
-    LEARNING_RATE: float = field(init=False) 0.001
-    EVALUATE: bool = field(init=False) True
-    INVARIANCE: bool = field(init=False) True
-    TENSOR_INVARIANTS: bool = field(init=False) True
-    TRAINING_FRACTION: float = field(init=False) 0.8
-    BATCH_SIZE: int = field(init=False) 1
+    N_EPOCHS: int = field(init=False)
+    LEARNING_RATE: float = field(init=False)
+    EVALUATE: bool = field(init=False)
+    INVARIANCE: bool = field(init=False)
+    TENSOR_INVARIANTS: bool = field(init=False)
+    TRAINING_FRACTION: float = field(init=False)
+    BATCH_SIZE: int = field(init=False)
  
     def load_config(self, config_path: Path) -> None:
 
@@ -32,7 +29,7 @@ class Config:
 
         # load yaml file to dictionary
         with open(config_path, 'r') as f:
-            tmp_config = yaml.load(stream=f, Loader=yaml.CLoader)
+            tmp_config = yaml.load(stream=f, Loader=yaml.Loader)
 
         # assuming values are dictionaries
         for v in tmp_config.values():
@@ -43,10 +40,28 @@ class Config:
 
         # remove any private fields
         _private_fields = [f for f in field_names if f.startswith('_')]
+        field_names.difference_update(_private_fields)
 
+        # update class attributes
+        for k, v in self._config.items():
 
+            if k not in field_names:
+                raise ValueError(f'Invalid Field: {k} with value {v}')
 
+            k_field = next(filter(lambda x: x.name == k, fields(self)))
+            setattr(self, k, k_field.type(v))
 
+            field_names.remove(k)
 
+        if len(field_names) > 0:
 
+            msg = 'Missing values in config file: \n'
+            for fname in field_names:
+                _field = next(filter(lambda x: x.name == fname, fields(self)))
+                msg += f'{_field.name}: {_field.type.__name__}\n'
 
+            raise ValueError(msg)
+
+    @property
+    def config(self) -> Dict[str, Any]:
+        return self._config
